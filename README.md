f9adcdc4eb88b265391ce098a3e1f9a549dec742
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-07-24 01:08:32 +0530

    required library installation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 poweredbyautistics
+
+## TECHNOLOGIES USED
+
+<table>
+   <tr>
+       <td>React</td>
+       <td>React Router</td>
+       <td>Material UI</td>
+       <td>React Redux</td>
+   </tr>
+</table>
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Foooter/Footer';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <section>poweredbyautistics</section>
+      <Header />
+      <hr />
+      {/* Tab Bar Routes */}
+      <Link to="/course">Course</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <hr />
+      <Footer />
     </div>
   );
 }
