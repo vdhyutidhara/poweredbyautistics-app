525697f8cc96ade42d65142e5877135580c943a3
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-08-06 17:20:26 +0530

    import reorder for netlify build

--- src/Components/Foooter/Footer.jsx
+++ src/Components/Foooter/Footer.jsx
@@ -5,7 +5,6 @@ import EmailIcon from "@mui/icons-material/Email";
 import PhoneIcon from "@mui/icons-material/Phone";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import Avatar from "@mui/material/Avatar";
 
 export default function Footer() {
