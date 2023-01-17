b08a0d60607b139bdb5ba59c20ef5a568f1a80e3
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2023-01-17 17:38:49 +0530

    About: Basic founder's Page Added

--- src/Components/Main/About.jsx
+++ src/Components/Main/About.jsx
@@ -4,9 +4,18 @@ import styles from "./About.module.css";
 export default function About() {
   return (
     <div className={styles.aboutContainer}>
-      <h1>About Us</h1>
-      <div className={styles.aboutImage}>
-        <img src="https://i.imgur.com/lYARCqI.jpeg" alt="AboutPageImage" />
+      <h1>Meet our founders</h1>
+      <div className={styles.cardContainer}>
+        <div className={styles.founderCard}>
+          <img src="http://www.autism-india.org/images/page/team/Sudanshu.jpg" alt="sudhanshu_img" />
+          <h2>Sudhanshu Grover</h2>
+
+        </div>
+        <div className={styles.founderCard}>
+          <img src="https://avatars.githubusercontent.com/u/4136334?v=4" alt="aniket_img" />
+          <h2>Aniket Kadam</h2>
+
+        </div>
       </div>
     </div>
   );
